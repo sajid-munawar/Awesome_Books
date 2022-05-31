bd531a6b7eb2afe1326b3038628ab24e6458d97e
Zelalem1222 <zelalemfissha394@gmail.com>
2022-05-31 08:10:59 +0300

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const booksContainer = document.querySelector('.books-container');
 const form = document.querySelector('form');
 
@@ -12,13 +14,74 @@ let books = [
   },
 ];
 
-function generateBooks({ title, author }) {
-  return `<ul>
-        <li>${title}</li>
-        <li>${author}</li>
-        <button>Remove</button>
-    </ul><hr>`;
+
+
+ class Books {
+  constructor(title , author) {
+    this.title = title;
+    this.author = author;
+  }
+
+   generateBooks({ title, author }) {
+    return `<ul>
+          <li>${this.title}</li>
+          <li>${this.author}</li>
+          <button>Remove</button>
+      </ul><hr>`;
+  }
+
+   addBook (e)  {
+    // e.preventDefault();
+    this.title = form.title.value.trim();
+     this.author = form.author.value.trim();
+    if (this.title && this.author) {
+      if (booksFromLocalStorage) {
+        booksFromLocalStorage.push({title:this.title , author: this.author });
+        localStorage.setItem('books', JSON.stringify(booksFromLocalStorage));
+        booksContainer.innerHTML = booksFromLocalStorage
+          .map((book) => generateBooks(book))
+          .join('');
+        form.reset();
+      } else {
+        books.push({title:this.title, author: this.author });
+        localStorage.setItem('books', JSON.stringify(books));
+        booksContainer.innerHTML = books
+          .map((book) => generateBooks(book))
+          .join('');
+        form.reset();
+      }
+    }
+
+    return this;
+  };
+
+   removeBook(e) {
+
+     
+    if (e.target.tagName === 'BUTTON') {
+      this.title = e.target.parentElement.firstElementChild.textContent;
+  
+      if (booksFromLocalStorage) {
+        booksFromLocalStorage = booksFromLocalStorage.filter(
+          (obj) => obj.title !== this.title,
+        );
+        localStorage.setItem('books', JSON.stringify(booksFromLocalStorage));
+        booksContainer.innerHTML = booksFromLocalStorage
+          .map((book) => generateBooks(book))
+          .join('');
+        form.reset();
+      } else {
+        books = books.filter((obj) => obj.title !== this.title);
+        localStorage.setItem('books', JSON.stringify(books));
+        booksContainer.innerHTML = books
+          .map((book) => generateBooks(book))
+          .join('');
+      }
+    }
+  };
+  
 }
+
 
 let booksFromLocalStorage = JSON.parse(localStorage.getItem('books'));
 
@@ -30,51 +93,9 @@ if (booksFromLocalStorage) {
   booksContainer.innerHTML = books.map((book) => generateBooks(book)).join('');
 }
 
-const addBook = (e) => {
-  e.preventDefault();
-  const title = form.title.value.trim();
-  const author = form.author.value.trim();
-  if (title && author) {
-    if (booksFromLocalStorage) {
-      booksFromLocalStorage.push({ title, author });
-      localStorage.setItem('books', JSON.stringify(booksFromLocalStorage));
-      booksContainer.innerHTML = booksFromLocalStorage
-        .map((book) => generateBooks(book))
-        .join('');
-      form.reset();
-    } else {
-      books.push({ title, author });
-      localStorage.setItem('books', JSON.stringify(books));
-      booksContainer.innerHTML = books
-        .map((book) => generateBooks(book))
-        .join('');
-      form.reset();
-    }
-  }
-};
+let bookClass = new Books();
+console.log(bookClass);
 
-const removeBook = (e) => {
-  if (e.target.tagName === 'BUTTON') {
-    const title = e.target.parentElement.firstElementChild.textContent;
 
-    if (booksFromLocalStorage) {
-      booksFromLocalStorage = booksFromLocalStorage.filter(
-        (obj) => obj.title !== title,
-      );
-      localStorage.setItem('books', JSON.stringify(booksFromLocalStorage));
-      booksContainer.innerHTML = booksFromLocalStorage
-        .map((book) => generateBooks(book))
-        .join('');
-      form.reset();
-    } else {
-      books = books.filter((obj) => obj.title !== title);
-      localStorage.setItem('books', JSON.stringify(books));
-      booksContainer.innerHTML = books
-        .map((book) => generateBooks(book))
-        .join('');
-    }
-  }
-};
-
-form.addEventListener('submit', addBook);
-booksContainer.addEventListener('click', removeBook);
+form.addEventListener('submit', bookClass.addBook());
+booksContainer.addEventListener('click', bookClass.removeBook());
